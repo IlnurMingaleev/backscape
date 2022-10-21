dda8189bed572532c3f2c3e7fe6fee8512fe5bad
Ilnur-web <ilmingaleev@yandex.ru>
2022-10-21 15:56:58 +0300

    Added on click events to buttons
    Worked on adaptive layout of elements in UI

--- Backscape/Assets/UI Example/Scripts/UIExample.cs
+++ Backscape/Assets/UI Example/Scripts/UIExample.cs
@@ -13,7 +13,7 @@ public class UIExample: EditorWindow
     private VisualTreeAsset listPage;
     private StyleSheet mutualStyle;
     private Action<Button> OnItemClicked;
-
+    private Action<Button> OnBackButtonClicked;
     private Dictionary<string, Button> uiButtons;
 
     private string currentPage;
@@ -26,16 +26,9 @@ public class UIExample: EditorWindow
     {
         UIExample uiExample = GetWindow<UIExample>();
         uiExample.titleContent = new GUIContent("UIExample");
-
-        
-
-    }
-    private void OnDestroy()
-    {
-
     }
 
-    // Load UXML files to UnityEditor window. One as default. Other for next button onklick events.
+    
     public void CreateGUI()
     {
         //Initialize UI buttons dictionary
@@ -43,79 +36,169 @@ public class UIExample: EditorWindow
 
 
 
-
+        // Load UXML files to UnityEditor window. One as default. Other for next button onklick events.
         mutualStyle = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/UI Example/uxml&uss/style.uss");
         tutorialPage = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/UI Example/uxml&uss/tutorial_page.uxml");
         videoPage = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/UI Example/uxml&uss/video_page.uxml");
         listPage = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/UI Example/uxml&uss/list_page.uxml");
         rootVisualElement.styleSheets.Add(mutualStyle);
 
-
-        rootVisualElement.Add(tutorialPage.Instantiate());
-        OnItemClicked = AddListenerToItemBtn;
-        rootVisualElement.Query<Button>("item").ForEach( OnItemClicked );
-        //uiButtons.Add("item", rootVisualElement.Q<Button>("item"));
-
-
-        /*videoPage = new VisualElement();
-        
-        videoPage.Add(videoPageVTA.Instantiate());
-        uiButtons.Add("menu_btn_back", videoPage.Q<Button>("menu_btn_back"));
-
-        listPage = new VisualElement();
-        
-        listPage.Add(listPageVTA.Instantiate());
-        uiButtons.Add("list_item", videoPage.Q<Button>("list_item"));
+        //Load first page of UI
+        LoadTutorialPage();
 
         
-        tutorialPage.styleSheets.Add(mutualStyle);
-        videoPage.styleSheets.Add(mutualStyle);
-        listPage.styleSheets.Add(mutualStyle);*/
-        //Button button = uiButtons["item"];
-        /*if (button != null)
-        {
-            button.clickable.clicked += ItemClicked;
-        }
-        else 
-        {
-            Debug.Log("Button is null");
-        }*/
-        
-        //["menu_btn_back"].RegisterCallback<MouseDownEvent>(BackButtonClicked);
+    }
+
+    #region OnClickEvent Listeners and their methods.
+
+    private void AddEventToButtonsByName(string name, Action<Button> action)
+    {
+        rootVisualElement.Query<Button>(name).ForEach(action);
 
     }
-    private void AddListenerToItemBtn(Button button) 
+    
+    /// <summary>
+    /// Add listener function to a button called item. Which located on tutorial page.
+    /// </summary>
+    /// <param name="button"></param>
+    private void AddListenerItemButton(Button button) 
     {
         button.clickable.clicked += ItemClicked;
     }
+    /// <summary>
+    /// Add listener to all back buttons in page
+    /// </summary>
+    /// <param name="button"></param>
+    private void AddListenerBackButton(Button button) 
+    {
+        button.clickable.clicked += BackButtonClicked;
+    }
 
+    private void AddListenerListItemBtn(Button button) 
+    {
+        button.clickable.clicked += ListItemClicked;
+    }
+
+    /// <summary>
+    /// Method called on Item clicked
+    /// </summary>
+    private void ItemClicked()
+    {
+        LoadListPage();
+    }
+
+    /// <summary>
+    /// Method called on list_item clicked
+    /// </summary>
+    private void ListItemClicked()
+    {
+        LoadVideoPage();
+    }
+
+    /// <summary>
+    /// Method called on back_btn and menu_btn_back clicked
+    /// </summary>
+    private void BackButtonClicked()
+    {
+        switch (currentPage)
+        {
+            case "listPage":
+                LoadTutorialPage();
+                break;
+            case "videoPage":
+                LoadListPage();
+                break;
+
+        }
+
+    }
+    #endregion
+
+    /// <summary>
+    /// Clear all pages from root visual element
+    /// </summary>
     private void ClearRootVisualElement() 
     {
         rootVisualElement.Clear();
     }
+
+    /// <summary>
+    /// Load new visual tree asset to the rootVisualElement. But before clear all pages
+    /// </summary>
+    /// <param name="treeAsset"></param>
     private void LoadVisuals(VisualTreeAsset treeAsset) 
     {
         ClearRootVisualElement();
         rootVisualElement.Add(treeAsset.Instantiate());
         rootVisualElement.styleSheets.Add(mutualStyle);
     }
-    private void ItemClicked() 
+
+    
+
+    /// <summary>
+    /// Load first page
+    /// </summary>
+    private void LoadTutorialPage() 
     {
-        LoadVisuals(listPage);
+        //Add on click events to all clickable items
+        LoadVisuals(tutorialPage);
+        currentPage = "tutorialPage";
+        OnItemClicked = null;
+        OnItemClicked += AddListenerItemButton;
+        AddEventToButtonsByName("item", OnItemClicked);
+        OnBackButtonClicked = null;
+        OnBackButtonClicked = AddListenerBackButton;
+        AddEventToButtonsByName("back_btn", OnBackButtonClicked);
+        AddEventToButtonsByName("menu_btn_back", OnBackButtonClicked);
+    }
+    
+    /// <summary>
+    /// Load third page
+    /// </summary>
+    private void LoadVideoPage() 
+    {
+        LoadVisuals(videoPage);
+        currentPage = "videoPage";
+        OnItemClicked = null;
+        //OnItemClicked += AddListenerListItemBtn;
+        //AddEventToButtonsByName("list_item", OnItemClicked);
+        OnBackButtonClicked = null;
+        OnBackButtonClicked = AddListenerBackButton;
+        AddEventToButtonsByName("back_btn", OnBackButtonClicked);
+        AddEventToButtonsByName("menu_btn_back", OnBackButtonClicked);
     }
 
-    private void BackButtonClicked() 
+    /// <summary>
+    /// Load Second page.
+    /// </summary>
+    private void LoadListPage() 
     {
-        switch (rootVisualElement.name) 
+        //Add on click events to all clickable items
+        LoadVisuals(listPage);
+        currentPage = "listPage";
+        OnItemClicked = null;
+        OnItemClicked += AddListenerListItemBtn;
+        AddEventToButtonsByName("list_item", OnItemClicked);
+        OnBackButtonClicked = null;
+        OnBackButtonClicked = AddListenerBackButton;
+        AddEventToButtonsByName("back_btn", OnBackButtonClicked);
+        AddEventToButtonsByName("menu_btn_back", OnBackButtonClicked);
+    }
+
+    private void Load(string name) 
+    {
+        switch (name)
         {
+            case "tutorial_page":
+                LoadTutorialPage();
+                break;
             case "list_page":
-                LoadVisuals(tutorialPage);
+                LoadListPage();
                 break;
             case "video_page":
-                LoadVisuals(listPage);
-                break ;
-            
+                LoadVideoPage();
+                break;
         }
-    
     }
+    
 }
